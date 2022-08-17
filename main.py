6a0534dc345752efba27f988d1977f88a55df4bb
FelixYale <dexteryale@gmail.com>
2022-08-17 02:11:06 +0100

    Latest

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import jwt
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
+#this is a test
 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
